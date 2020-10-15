449c97ca0b501eb3732dad0a851198f2bc1c18f8
Blueskyfish <blueskyfish@blueskyfish.de>
2020-10-15 20:36:34 +0200

    (background) improve the bootstrap process

--- apps/background/src/main.ts
+++ apps/background/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { fromEnv } from '@blue-paper/server-commons';
@@ -10,15 +5,13 @@ import { fromEnv } from '@blue-paper/server-commons';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-
   const port = fromEnv('background_port', '3333').asNumber;
+  const host = fromEnv('background_host', 'localhost').asString;
 
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
 
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  await app.listen(port, host, () => {
+    Logger.log(`Listening at http://${host}:${port}/`);
   });
 }
 
