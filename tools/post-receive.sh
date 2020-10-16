a8a9dbf09078cda44be70c15e84782cc065851f6
BlueSkyFish <blueskyfish@noreply.example.org>
2020-10-16 15:54:48 +0200

    (tools) update githook (#2)
    (tools) update the branch
    
    (tools) update token (previous token is invalid)
    
    Co-authored-by: Blueskyfish <blueskyfish@blueskyfish.de>
    Reviewed-on: https://blueskyfish.de/git/blueskyfish/blue-paper/pulls/2

--- tools/post-receive.sh
+++ tools/post-receive.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 while read oldrev newrev refname
 do
-    branch=$(git rev-parse --symbolic --abbrev-ref $refname)
-    if [ "master" = "$branch" ]; then
-        git push --mirror --quiet https://blueskfish:d57b5098dd0475e4dab4f2e2d1c3417773e2d77a@github.com/blueskyfish/blue-paper.git &> /dev/null
-        echo "push to github the master branch"
-    fi
+  branch=$(git rev-parse --symbolic --abbrev-ref $refname)
+  if [ "master" = "$branch" ]; then
+    # Previous token is invalidate
+    git push master --quiet https://user:token@github.com/blueskyfish/blue-paper.git &> /dev/null
+    echo "push to github the master branch"
+  fi
 done
