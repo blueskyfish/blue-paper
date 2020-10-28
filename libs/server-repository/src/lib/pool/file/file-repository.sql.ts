35d7f7f6d74789b49a52720b699b1260f267035a
Blueskyfish <blueskyfish@blueskyfish.de>
2020-10-28 16:07:32 +0100

    (Repository) get the image list from menu and group

--- libs/server-repository/src/lib/pool/file/file-repository.sql.ts
+++ libs/server-repository/src/lib/pool/file/file-repository.sql.ts
@@ -1,15 +1,24 @@
 import { DbFile, NL } from '../repository.names';
 
+const SELECT_FILE = 'SELECT file_id AS id, menu_id AS menuId, group_id AS groupId, filename, mimetype, etag, size';
+
 export const SQL_FIND_FILE_BY_GROUP_AND_FILENAME = [
-  'SELECT file_id AS id, menu_id AS menuId, group_id AS groupId, filename, mimetype, etag, size', NL,
+  SELECT_FILE, NL,
   'FROM ', DbFile, NL,
   'WHERE group_id = {groupId} AND filename = {filename}'
 ].join('');
 
 export const SQL_FIND_FILE_BY_ID = [
-  'SELECT file_id AS id, menu_id AS menuId, group_id AS groupId, filename, mimetype, etag, size', NL,
+  SELECT_FILE, NL,
   'FROM ', DbFile, NL,
   'WHERE file_id = {fileId}'
+].join('');
+
+export const SQL_SELECT_IMAGE_LIST_FROM_MENU_GROUP = [
+  SELECT_FILE, NL,
+  'FROM ', DbFile, NL,
+  'WHERE menu_id = {menuId} AND group_id = {groupId}', NL,
+  'ORDER BY creation'
 ].join('');
 
 
--- libs/server-repository/src/lib/pool/file/file-repository.ts
+++ libs/server-repository/src/lib/pool/file/file-repository.ts
@@ -1,5 +1,10 @@
 import { SubRepository } from '../sub-repository';
-import { SQL_FIND_FILE_BY_GROUP_AND_FILENAME, SQL_FIND_FILE_BY_ID, SQL_INSERT_FILE } from './file-repository.sql';
+import {
+  SQL_FIND_FILE_BY_GROUP_AND_FILENAME,
+  SQL_FIND_FILE_BY_ID,
+  SQL_INSERT_FILE,
+  SQL_SELECT_IMAGE_LIST_FROM_MENU_GROUP
+} from './file-repository.sql';
 import { IDbFile } from './file.entity';
 import { IDbInsertFile } from './insert-file.entity';
 
@@ -15,5 +20,9 @@ export class FileRepository extends SubRepository {
 
   async insertFile(values: IDbInsertFile): Promise<number> {
     return await this.conn.insert(SQL_INSERT_FILE, values);
+  }
+
+  async getImageListFromMenuGroup(menuId: number, groupId: number): Promise<IDbFile[]> {
+    return await this.conn.select<IDbFile>(SQL_SELECT_IMAGE_LIST_FROM_MENU_GROUP, {menuId, groupId});
   }
 }
