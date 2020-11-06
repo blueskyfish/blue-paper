baf9e4371ce307966bd0a26e2e83ebd086c13794
Blueskyfish <blueskyfish@blueskyfish.de>
2020-11-06 14:58:24 +0100

    (Authorization) add openapi declarations

--- libs/server-authentication/src/index.ts
+++ libs/server-authentication/src/index.ts
@@ -1,5 +1,8 @@
 export * from './lib/authentication.config';
 export * from './lib/authentication.module';
 
+// Auth Key
+export { API_KEY_NAME, HTTP_AUTH_HEADER, API_KEY_SECURITY } from './lib/authentication.const';
+
 // Services
 export * from './lib/services';
