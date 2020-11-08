ea060094547c64dcb419e3773525474a7829c087
Blueskyfish <blueskyfish@blueskyfish.de>
2020-11-08 18:48:23 +0100

    (Paper Bureau) configuration and lang "en"

--- apps/paper-editor/src/app/app.config.ts
+++ apps/paper-editor/src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
+import { IStoreHttpConfig } from '@blue-paper/ui-store-editor';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { TranslateLoader, TranslateModuleConfig } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
 
 export function httpTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/lang-', '.json');
@@ -12,8 +15,25 @@ export function buildTranslateConfig(): TranslateModuleConfig {
     loader: {
       provide: TranslateLoader,
       useFactory: (httpTranslateLoader),
-      deps: [HttpClient]
+      deps: [ HttpClient ]
     },
-    defaultLanguage: 'en',
-  }
+    defaultLanguage: 'en'
+  };
+}
+
+export function buildHttpConfig(): IStoreHttpConfig {
+  return {
+    baseApiUrl: environment.baseApiUrl,
+    headerName: 'x-blue-paper'
+  };
+}
+
+
+export function configStoreDev(): any[] {
+  return environment.production ? []
+    : [
+      StoreDevtoolsModule.instrument({
+        name: 'NgRx Paper Editor'
+      })
+    ];
 }
--- apps/paper-editor/src/app/app.module.ts
+++ apps/paper-editor/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { forwardRef, NgModule } from '@angular/core';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { UiCommonsModule } from '@blue-paper/ui-commons';
 import { UiComponentsModule } from '@blue-paper/ui-components';
+import { StoreHttpService, UiStoreEditorModule } from '@blue-paper/ui-store-editor';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
-import { buildTranslateConfig } from './app.config';
+import { buildHttpConfig, buildTranslateConfig, configStoreDev } from './app.config';
 import { ROUTES } from './app.routing';
 import { HomeViewComponent, LoginViewComponent } from './views';
 
@@ -33,12 +34,21 @@ const components = [
 
     TranslateModule.forRoot(buildTranslateConfig()),
 
+    ...configStoreDev(),
+
     MatIconModule,
 
     UiCommonsModule,
     UiComponentsModule,
+    UiStoreEditorModule.forRoot(buildHttpConfig()),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useExisting: forwardRef(() => StoreHttpService),
+      multi: true,
+    }
+  ],
   bootstrap: [
     AppComponent
   ],
